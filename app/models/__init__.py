f4e060a06d4eff9594f6f472fb28554ed208be05
thengel sec <ssuk83158315@gmail.com>
2026-03-23 16:45:42 +0900

    feat: 판례/법령/양형기준 DB 모델 추가 및 수정
    - cases_raw, sentencing, statutees 추가
    - cases 컬럼 추가

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,10 @@
 # 모델 파일들을 한곳에서 불러오는 파일
 # 여기서 import 해야 create_tables()가 어떤 테이블을 만들지 알 수 있음
-from app.models.case import Case
+from app.models.cases import Cases
+from app.models.cases_raw import CasesRaw
+from app.models.sentencing import Sentencing
+from app.models.statutes import Statutes
 from app.models.user import User
+from app.models.user_case import UserCase
 
-__all__ = ["Case", "User", "UserCase"]
+__all__ = ["Cases", "CasesRaw", "Sentencing", "Statutes", "User", "UserCase"]
